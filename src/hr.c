0e8568ff31417fe1c6f0b06c9b16fc5bcd7df497
Dylan Simon <dylan@dylex.net>
2011-06-14 23:44:10 +0000

    prepare for hackage

--- src/hr.c
+++ src/hr.c
@@ -1,6 +1,7 @@
 #include <HsFFI.h>
 
 double r_ValueOfNA() { 
+	/* R_NaReal is not initialized until runtime, so we can't use it. */
 	static uint64_t ValueOfNA = 0x7ff00000000007a2;
 	return *(double *)&ValueOfNA; 
 }
